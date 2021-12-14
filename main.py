9d7d0cfab8b5b50bc05439a2ecbec1c3630b738c
Riccardo Orion Feingold <riccardofeingold@gmail.com>
2021-12-14 08:50:47 +0100

    setup selenium and chromedriver

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 from selenium import webdriver
 from time import sleep
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+tel = os.environ.get("tel")
+
+chrome_driver_path = os.environ.get("chrome-driver-path")
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("http://www.tinder.com")
+
+sleep(2)
+login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
