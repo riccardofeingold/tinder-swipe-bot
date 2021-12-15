592ecbed1b7e224963d10ff97cb00534eb7173b6
Riccardo Feingold <riccardofeingold@gmail.com>
2021-12-15 15:00:07 +0100

    first version of swipe bot works, need to improve login precedure with apple login

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+import pyautogui
 from time import sleep
 import os
 from dotenv import load_dotenv
+import pyperclip
 
 load_dotenv()
 
 username = os.environ.get("username")
 pwd = os.environ.get("pwd")
+pyperclip.copy(pwd)
 
 chrome_driver_path = os.environ.get("chrome-driver-path")
 options = webdriver.ChromeOptions()
@@ -19,48 +22,49 @@ options.add_experimental_option('prefs', {
 })
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 
-driver.get("http://www.tinder.com")
+driver.get("http://www.bumble.com")
 
 sleep(4)
-accept_cookies = driver.find_element(By.XPATH, '//*[@id="c1276625974"]/div/div[2]/div/div/div[1]/button')
-accept_cookies.click()
-
-sleep(4)
-login_button = driver.find_element(By.XPATH, '//*[@id="c1276625974"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+login_button = driver.find_element(By.XPATH, '//*[@id="page"]/div/div/div[1]/div/div[2]/div/div/div/div[2]/div[1]/div/div[2]/a')
 login_button.click()
 
 sleep(4)
-tel_login = driver.find_element(By.XPATH, '//*[@id="c1497966410"]/div/div/div[1]/div/div[3]/span/div[3]/button')
-tel_login.click()
+apple_login = driver.find_element(By.XPATH, '//*[@id="main"]/div/div[1]/div[2]/main/div/div[3]/form/div[1]/div/div[1]/div')
+apple_login.click()
 
-sleep(8)
-tel_input = driver.find_element(By.XPATH, '//*[@id="c1497966410"]/div/div/div[1]/div[2]/div/input')
-tel_input.send_keys(tel)
-tel_input.send_keys(Keys.ENTER)
+base_window = driver.window_handles[0]
+apple_login_window = driver.window_handles[1]
+driver.switch_to.window(apple_login_window)
 
-input("hit enter if you are ready!")
+sleep(4)
+username_input = driver.find_element(By.XPATH, '//*[@id="account_name_text_field"]')
+username_input.send_keys(username)
+sleep(1)
+username_input.send_keys(Keys.ENTER)
 
-sleep(10)
+sleep(4)
+pwd_input = driver.find_element(By.XPATH, '//*[@id="password_text_field"]')
+input("enter your password")
+sleep(2)
+pwd_input.send_keys(Keys.ENTER)
 
-for _ in range(10000):
+input("Enter verification code")
+
+sleep(4)
+
+driver.switch_to.window(base_window)
+sleep(2)
+
+for _ in range(25):
     sleep(2)
     try:
         like_button = driver.find_element(By.XPATH,
-            '//*[@id="c1276625974"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button')
+            '//*[@id="main"]/div/div[1]/main/div[2]/div/div/span/div[2]/div/div[2]/div/div[3]/div/div[1]/span')
         like_button.click()
 
     # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
     except ElementClickInterceptedException:
-        try:
-            match_popup = driver.find_element(By.XPATH, ".itsAMatch a")
-            match_popup.click()
-
-            #super_like_popup = driver.find_element(By.XPATH, '//*[@id="c1497966410"]/div/div/button[2]')
-            #super_like_popup.click()
-
-        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
-        except NoSuchElementException:
-            sleep(3)
+        sleep(10)
 
 
 driver.quit()
